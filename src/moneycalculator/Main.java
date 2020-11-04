c211ff4f6ba73c5328f59a8bc82e2cb5471a69c9
markensi <markensi9@outlook.com>
2020-11-04 19:57:08 +0000

    Money Calculator

---
+++ src/moneycalculator/Main.java
@@ -0,0 +1,20 @@
+package moneycalculator;
+
+import moneycalculator.control.CalculateCommand;
+import moneycalculator.persistence.CurrencyListLoader;
+import moneycalculator.persistence.files.FileCurrencyListLoader;
+import moneycalculator.persistence.rest.RestExchangeRateLoader;
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CurrencyListLoader currencyLoader = new FileCurrencyListLoader("currencies");
+        RestExchangeRateLoader exchangeRateLoader = new RestExchangeRateLoader();
+        MainFrame mainFrame = new MainFrame(currencyLoader.currencies());
+        mainFrame.add(new CalculateCommand(mainFrame.getMoneyDialog(), mainFrame.getMoneyDisplay(), exchangeRateLoader));
+    }  
+}
